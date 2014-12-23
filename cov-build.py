ef8abadeba57a7ceeba4d8a83ee2f829abb29e48
raof01 <raof01@gmail.com>
2014-12-23 23:20:41 +0800

    Update cov-build.py
    Changed to use tk.Listbox for output window. Call Listbox.update() to update the output

--- cov-build.py
+++ cov-build.py
@@ -55,7 +55,12 @@ class Application(tk.Frame):
         self.__create_widgets()
 
     def __clear_output(self):
-        self.__lbOutput.delete(0, self.__lbOutput.size() - 1)
+        self.__lb_output.delete(0, self.__lb_output.size() - 1)
+    
+    def __output(self, str):
+        self.__lb_output.insert(tk.END, str)
+        self.__lb_output.yview(tk.END)
+        self.__lb_output.update()
 
     def __choose_build_tool_dialog(self):
         self.__build_cmd = filedialog.askopenfilename()
@@ -66,24 +71,11 @@ class Application(tk.Frame):
     def __choose_coverity_dialog(self):
         self.__cov_cmd_dir = filedialog.askdirectory()
         if self.__cov_cmd_dir:
-            self.__lblCov['text']=self.__cov_cmd_dir
+            self.__lbl_cov['text']=self.__cov_cmd_dir
             pass
 
 
-    def __run_cmd(self, cmd, wd=None):
-        cwd = os.getcwd()
-        if wd:
-            os.chdir(wd)
-        try:
-            return subprocess.check_call(cmd)
-        except (KeyboardInterrupt, subprocess.CalledProcessError) as e:
-            self.__lbOutput.insert(tk.END, 'ERROR!')
-            return -1
-        if wd:
-            os.chdir(cwd)
-
-
-    def __run_cmd_redirected(self, cmd, wd=None):
+    def __run_cmd_in_pipe(self, cmd, wd=None):
         cwd = os.getcwd()
         if wd:
             os.chdir(wd)
@@ -102,16 +94,13 @@ class Application(tk.Frame):
             while not stdout_reader.eof or not stderr_reader.eof:
                 # Show what we received from standard output.
                 while not stdout_queue.empty():
-                    #if self.__lbOutput.size() > 9999:
-                    #    self.__clear_output()
-                    self.__lbOutput.insert(tk.END, stdout_queue.get())
+                    if self.__lb_output.size() > 9999:
+                        self.__clear_output()
+                    self.__output(stdout_queue.get().decode('utf-8'))
 
                 # Show what we received from standard error.
                 #for line in iter(stderr_queue.get, None):
                 #    print(str(line))
-
-                # Sleep a bit before asking the readers again.
-                time.sleep(.5)
 
             # Let's be tidy and join the threads we've started.
             stdout_reader.join()
@@ -121,7 +110,7 @@ class Application(tk.Frame):
             process.stdout.close()
             process.stderr.close()
         except (KeyboardInterrupt, subprocess.CalledProcessError) as e:
-            self.__lbOutput.insert(tk.END, 'ERROR!')
+            self.__output('ERROR!')
             return -1
         if wd:
             os.chdir(cwd)
@@ -129,48 +118,48 @@ class Application(tk.Frame):
     def __build(self):
         if not self.__build_cmd:
             self.__clear_output()
-            self.__lbOutput.insert(tk.END, 'Invailid build command')
+            self.__output('Invailid build command')
         else:
             cmd = [os.path.basename(self.__build_cmd), self.__proj]
-            self.__run_cmd(cmd, os.chdir(os.path.dirname(self.__build_cmd)))
+            self.__run_cmd_in_pipe(cmd, os.chdir(os.path.dirname(self.__build_cmd)))
 
     def __cov_build(self):
         if not self.__cov_cmd_dir:
             self.__clear_output()
-            self.__lbOutput.insert(tk.END, 'Invailid cov command')
+            self.__output('Invailid cov command')
         else:
             cov_dir = os.path.dirname(self.__proj) + os.sep + 'Debug' + os.sep + 'cov'
             shutil.rmtree(cov_dir, ignore_errors=True)
             os.makedirs(cov_dir)
             cmd = [self.__cov_cmd_dir + os.sep + 'cov-build','--dir', cov_dir, os.path.basename(self.__build_cmd), self.__proj]
-            self.__lbOutput.insert(tk.END, '------------------------------cov-build in progress------------------------------')
-            self.__run_cmd(cmd, os.path.dirname(self.__build_cmd))
-            self.__lbOutput.insert(tk.END, '------------------------------cov-build done------------------------------')
-            self.__lbOutput.insert(tk.END, '------------------------------cov-analyze in progress------------------------------')
+            self.__output('------------------------------cov-build in progress------------------------------')
+            self.__run_cmd_in_pipe(cmd, os.path.dirname(self.__build_cmd))
+            self.__output('------------------------------cov-build done------------------------------')
+            self.__output('------------------------------cov-analyze in progress------------------------------')
 
             cmd = [self.__cov_cmd_dir + os.sep + 'cov-analyze', '--dir', cov_dir,
                    '--enable-constraint-fpp', '--enable-callgraph-metrics', '-j','2']
-            self.__run_cmd(cmd)
-            self.__lbOutput.insert(tk.END, '------------------------------cov-analyze done------------------------------')
-            self.__lbOutput.insert(tk.END, '------------------------------cov-format-errors in progress------------------------------')
+            self.__run_cmd_in_pipe(cmd)
+            self.__output('------------------------------cov-analyze done------------------------------')
+            self.__output('------------------------------cov-format-errors in progress------------------------------')
             cmd = [self.__cov_cmd_dir + os.sep + 'cov-format-errors', '--dir', cov_dir]
-            self.__run_cmd(cmd)
-            self.__lbOutput.insert(tk.END, '------------------------------cov-format-errors done------------------------------')
+            self.__run_cmd_in_pipe(cmd)
+            self.__output('------------------------------cov-format-errors done------------------------------')
             webbrowser.open(cov_dir + os.sep + 'output\\errors\\index.html')
 
     def __choose_project(self):
         self.__proj = filedialog.askopenfilename()
         if self.__proj:
-            self.__lblProj['text'] = self.__proj
+            self.__lbl_project['text'] = self.__proj
 
     def __create_widgets(self):
         self.pack(fill=tk.BOTH, expand=1)
 
-        self.__btnProj = tk.Button(self, text='Project')
-        self.__btnProj.grid(row=Row.PROJECT.value, column=Column.BTN.value, sticky=tk.W)
-        self.__btnProj['command'] = self.__choose_project
-        self.__lblProj = tk.Label(self)
-        self.__lblProj.grid(row=Row.PROJECT.value, column=Column.LABEL.value, sticky=tk.W)
+        self.__btn_project = tk.Button(self, text='Project')
+        self.__btn_project.grid(row=Row.PROJECT.value, column=Column.BTN.value, sticky=tk.W)
+        self.__btn_project['command'] = self.__choose_project
+        self.__lbl_project = tk.Label(self)
+        self.__lbl_project.grid(row=Row.PROJECT.value, column=Column.LABEL.value, sticky=tk.W)
         
         self.__btn_build_cmd = tk.Button(self, text = 'BuildCmd')
         self.__btn_build_cmd.grid(row=Row.BUILDTOOL.value, column=Column.BTN.value, sticky=tk.W)
@@ -179,33 +168,32 @@ class Application(tk.Frame):
         self.__lbl_build_cmd['text'] = 'Select Build command'
         self.__lbl_build_cmd.grid(row=Row.BUILDTOOL.value, column=Column.LABEL.value, sticky=tk.W)
 
-        self.__btnCov = tk.Button(self, text='coverity')
-        self.__btnCov.grid(row=Row.COV.value, column=Column.BTN.value, sticky=tk.W)
-        self.__btnCov['command'] = self.__choose_coverity_dialog
-        self.__lblCov = tk.Label(self)
-        self.__lblCov['text'] = 'Select coverity BIN directory'
-        self.__lblCov.grid(row=Row.COV.value, column=Column.LABEL.value, sticky=tk.W)
+        self.__btn_cov = tk.Button(self, text='coverity')
+        self.__btn_cov.grid(row=Row.COV.value, column=Column.BTN.value, sticky=tk.W)
+        self.__btn_cov['command'] = self.__choose_coverity_dialog
+        self.__lbl_cov = tk.Label(self)
+        self.__lbl_cov['text'] = 'Select coverity BIN directory'
+        self.__lbl_cov.grid(row=Row.COV.value, column=Column.LABEL.value, sticky=tk.W)
 
-        self.__btnBuild = tk.Button(self, text='Build')
-        self.__btnBuild['command'] = self.__build
-        self.__btnBuild.grid(row=Row.BUILD.value, column=Column.BTN.value, sticky=tk.W)
+        self.__btn_build = tk.Button(self, text='Build')
+        self.__btn_build['command'] = self.__build
+        self.__btn_build.grid(row=Row.BUILD.value, column=Column.BTN.value, sticky=tk.W)
 
-        self.__btnCovBuild = tk.Button(self, text='CovBuild')
-        self.__btnCovBuild['command'] = self.__cov_build
-        self.__btnCovBuild.grid(row=Row.COVBUILD.value, column=Column.BTN.value, sticky=tk.W)
+        self.__btn_cov_build = tk.Button(self, text='CovBuild')
+        self.__btn_cov_build['command'] = self.__cov_build
+        self.__btn_cov_build.grid(row=Row.COVBUILD.value, column=Column.BTN.value, sticky=tk.W)
 
-        self.__xscrollBar = tk.Scrollbar(self, orient=tk.HORIZONTAL)
-        self.__xscrollBar.grid(row=Row.XSCROLL.value, column=Column.BTN.value, columnspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
-        self.__yscrollBar = tk.Scrollbar(self)
-        self.__yscrollBar.grid(row=Row.OUTPUT.value, column=Column.YSCROLL.value, sticky=tk.W+tk.E+tk.N+tk.S)
+        self.__xscroll_bar = tk.Scrollbar(self, orient=tk.HORIZONTAL)
+        self.__xscroll_bar.grid(row=Row.XSCROLL.value, column=Column.BTN.value, columnspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
+        self.__yscroll_bar = tk.Scrollbar(self)
+        self.__yscroll_bar.grid(row=Row.OUTPUT.value, column=Column.YSCROLL.value, sticky=tk.W+tk.E+tk.N+tk.S)
         
-        self.__lbOutput = tk.Text(self, yscrollcommand = self.__yscrollBar.set, xscrollcommand = self.__xscrollBar.set)
-        self.__lbOutput['width'] = 140
-        self.__lbOutput['height'] = 20
-        self.__lbOutput.config(state='disabled')
-        self.__lbOutput.grid(row=Row.OUTPUT.value, column=Column.BTN.value, columnspan=2)
-        self.__yscrollBar.config(command = self.__lbOutput.yview)
-        self.__xscrollBar.config(command = self.__lbOutput.xview)
+        self.__lb_output = tk.Listbox(self, activestyle='none', yscrollcommand = self.__yscroll_bar.set, xscrollcommand = self.__xscroll_bar.set)
+        self.__lb_output['width'] = 140
+        self.__lb_output['height'] = 30
+        self.__lb_output.grid(row=Row.OUTPUT.value, column=Column.BTN.value, columnspan=2)
+        self.__yscroll_bar.config(command = self.__lb_output.yview)
+        self.__xscroll_bar.config(command = self.__lb_output.xview)
 
 
 if __name__ == '__main__':
