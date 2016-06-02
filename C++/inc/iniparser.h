8c862d8595bca836baf5fd881e882e0d32ab7cad
Felix Rao <raof01@gmail.com>
2016-06-02 22:36:13 +0800

    Create dummy files

---
+++ C++/inc/iniparser.h
@@ -0,0 +1,9 @@
+#ifndef _INI_PARSER_H
+#define _INI_PARSER_H
+
+class IniParser
+{
+
+};
+
+#endif
---
+++ C++/tests/iniparser_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "iniparser.h"
+
+TEST(IniParser, Foo)
+{
+    IniParser iniParser;
+    ASSERT_TRUE(true);
+}
