c0f3c82752090b7eb12c54033246504aa2a25108
Felix Rao <raof01@gmail.com>
2016-09-04 22:29:38 +0800

    Add unittest

--- python/MySqlAccess.py
+++ python/MySqlAccess.py
@@ -10,33 +10,24 @@ default_port = 8889
 class MySqlAccess:
     '''
     Wrapper class to access MySQL DB using mysql connector/python
-    '''    
+    '''
     def __init__(self, db_host = default_host, db_port = default_port):
-        self.__conn = MySQLConnection(host = db_host,
+        self._conn = MySQLConnection(host = db_host,
                                       port = db_port)
     
     def connect(self, user_name = None, user_passwd = None):
-        if (user_name is None) and (user_passwd is None):
+        if (user_name is None) or (user_passwd is None):
             return None
-        self.__conn.connect(user = user_name, password = user_passwd)
+        self._conn.connect(user = user_name, password = user_passwd)
         return 0
     
     def disconnect(self):
-        self.__conn.close()
+        self._conn.close()
 
     def query(self, sql_stmt = None):
-        cursor = self.__conn.cursor()
+        cursor = self._conn.cursor()
         query = sql_stmt
         cursor.execute(query)
         for row in cursor:
             print(row)
         cursor.close()
-
-if __name__ == '__main__':
-    dbAccess = MySqlAccess()
-    if dbAccess.connect('test', 'test123') is None:
-        print("error connecting")
-
-    select_sql = '''SELECT * FROM test_DB.Products'''
-    dbAccess.query(select_sql)
-    dbAccess.disconnect()
---
+++ python/unittests/MySqlAccessTest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+from unittest import mock
+
+sys.path.append('..')
+from MySqlAccess import MySqlAccess
+
+class MySqlAccessTest(unittest.TestCase):
+    def setUp(self):
+        self.__db_access = MySqlAccess()
+        self.__use_mock = False
+    
+    def tearDown(self):
+        if not self.__use_mock:
+            self.__db_access.disconnect()
+    
+    def __mock_mysql_db(self):
+        self.__use_mock = True
+        self.__db_access.disconnect()
+        self.__db_access._conn = mock.Mock()
+    
+    def __mock_mysql_db_connect(self):
+        self.__db_access._conn.connect = mock.Mock()
+    
+    def test_connect_return_none_when_user_name_is_none(self):
+        self.assertIsNone(self.__db_access.connect(user_passwd = 'passwd'))
+
+    def test_connect_return_none_when_user_passwd_is_none(self):
+        self.assertIsNone(self.__db_access.connect(user_name = 'user_name'))
+
+    def test_connect_return_none_when_user_name_and_passwd_is_provided(self):
+        self.__mock_mysql_db()
+        self.__mock_mysql_db_connect()
+        self.assertEqual(0, self.__db_access.connect(user_name = 'user_name',
+                                                   user_passwd = 'passwd'))
+
+    def test_query_to_real_db(self):
+        '''
+        WARNING!!! This should have MySQL DB set up
+        '''
+        self.__db_access.connect('test', 'test123')
+        select_sql = '''SELECT * FROM test_DB.Products'''
+        self.__db_access.query(select_sql)
+
+if __name__ == '__main__':
+    unittest.main()
