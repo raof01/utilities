333fc7dea333d39bf24dce62112cee5e5754bc1f
Felix Rao <raof01@gmail.com>
2016-09-20 08:36:18 +0800

    Add disconnect menu

--- python/PyQtDb/MainWindow.py
+++ python/PyQtDb/MainWindow.py
@@ -14,70 +14,9 @@ if platform.system() == 'Darwin':
 
 class MainWindow(QMainWindow):
     
-    def __init__(self):
-        super().__init__()
-        self.__initConstants()
-        self.initUI()
-        self.__dbAccess = None
-        
-    def __initConstants(self):
-        self.__TITLE = 'MySqlDbClient'
-        self.__CONNECT_PNG = 'connect.png'
-        self.__EXIT_PNG = 'exit.png'
-        self.__CONNECT_ACTION = '&Connect...'
-        self.__EXIT_ACTION = '&Exit'
-        self.__CONNECT_TOOLTIP = 'Connect to Database'
-        self.__EXIT_TOOLTIP = 'Exit Application'
-        self.__EXIT_SHORTCUT = 'Ctrl+Q'
-        self.__FILE_MENU = '&File'
-        self.__OSX_NAME = 'Darwin'
-        self.__STATUS_READY = 'Ready'
-        self.__STATUS_CONNECTED = 'Connected to '
-        self.__STATUS_CONNECT_FAILED = 'Fail to connected to '
-        self.__WINDOW_WIDTH = 600
-        self.__WINDOW_HEIGHT = 600
-        
-    def initUI(self):
-        self.__initMenuBar()
-        self.statusBar().showMessage(self.__STATUS_READY)
-        self.__moveToCenter()
-        self.setWindowTitle(self.__TITLE)
-
-    def __moveToCenter(self):
-        rect = qApp.desktop().availableGeometry(self)
-        center = rect.center()
-        self.setGeometry(0, 0, self.__WINDOW_WIDTH, self.__WINDOW_HEIGHT)
-        self.move(center.x() - self.__WINDOW_HEIGHT * 0.5, center.y() - self.__WINDOW_HEIGHT * 0.5)
-
-    def __createConnectAction(self):
-        connectAction = QAction(QIcon(self.__CONNECT_PNG), self.__CONNECT_ACTION, self)
-        connectAction.setStatusTip(self.__CONNECT_TOOLTIP)
-        connectAction.triggered.connect(self.__openConnectionDialog)
-        return connectAction
-
-    def __createQuitAction(self):
-        quitAction = QAction(QIcon(self.__EXIT_PNG), self.__EXIT_ACTION, self)
-        quitAction.setShortcut(self.__EXIT_SHORTCUT)
-        quitAction.setStatusTip(self.__EXIT_TOOLTIP)
-        quitAction.triggered.connect(self.quit)
-        return quitAction
-
-    def __initMenuBar(self):
-        menubar = self.menuBar()
-        if platform.system() == self.__OSX_NAME:
-            menubar.setNativeMenuBar(False) # Very important for Mac OS X
-        fileMenu = menubar.addMenu(self.__FILE_MENU)
-        fileMenu.addAction(self.__createConnectAction())
-        fileMenu.addAction(self.__createQuitAction())
-
-    def __openConnectionDialog(self):
-        self.__connDialog = DialogConnectToDataBase()
-        self.__connDialog.onDbConnected.connect(self.saveDbConnection)
-        self.__connDialog.onDbConnectedFailed.connect(self.reportDbConnectionFailure)
-        self.__connDialog.show()
-
     @pyqtSlot(str, MySqlAccess) # TODO
     def saveDbConnection(self, msg, dbAccess):
+        self.__setConnectionActionsEnabled(False)
         self.statusBar().showMessage(self.__STATUS_CONNECTED + msg)
         self.__dbAccess = dbAccess
         if self.__dbAccess:
@@ -95,9 +34,91 @@ class MainWindow(QMainWindow):
 
     @pyqtSlot()
     def quit(self):
+        self.__disconnect()
+        qApp.quit()
+
+    def __disconnect(self):
         if not self.__dbAccess is None:
             self.__dbAccess.disconnect()
-        qApp.quit()
+            self.__setConnectionActionsEnabled(True)
+            self.statusBar().showMessage(self.__STATUS_DISCONNECTED)
+    
+    def __setConnectionActionsEnabled(self, enabled):
+        self.__disconnAction.setEnabled(not enabled)
+        self.__connectAction.setEnabled(enabled)
+
+    def __init__(self):
+        super().__init__()
+        self.__initConstants()
+        self.initUI()
+        self.__dbAccess = None
+        
+    def __initConstants(self):
+        self.__TITLE = 'MySqlDbClient'
+        self.__CONNECT_PNG = 'connect.png'
+        self.__DISCONNECT_PNG = 'disconnect.png'
+        self.__EXIT_PNG = 'exit.png'
+        self.__CONNECT_ACTION = '&Connect...'
+        self.__DISCONNECT_ACTION = '&Disconnect'
+        self.__EXIT_ACTION = '&Exit'
+        self.__CONNECT_TOOLTIP = 'Connect to Database'
+        self.__EXIT_TOOLTIP = 'Exit Application'
+        self.__EXIT_SHORTCUT = 'Ctrl+Q'
+        self.__FILE_MENU = '&File'
+        self.__OSX_NAME = 'Darwin'
+        self.__STATUS_READY = 'Ready'
+        self.__STATUS_CONNECTED = 'Connected to '
+        self.__STATUS_CONNECT_FAILED = 'Fail to connected to '
+        self.__STATUS_DISCONNECTED = 'Disconnected'
+        self.__WINDOW_WIDTH = 600
+        self.__WINDOW_HEIGHT = 600
+        
+    def initUI(self):
+        self.__initMenuBar()
+        self.statusBar().showMessage(self.__STATUS_READY)
+        self.__moveToCenter()
+        self.setWindowTitle(self.__TITLE)
+
+    def __moveToCenter(self):
+        rect = qApp.desktop().availableGeometry(self)
+        center = rect.center()
+        self.setGeometry(0, 0, self.__WINDOW_WIDTH, self.__WINDOW_HEIGHT)
+        self.move(center.x() - self.__WINDOW_HEIGHT * 0.5, center.y() - self.__WINDOW_HEIGHT * 0.5)
+
+    def __createConnectAction(self):
+        self.__connectAction = QAction(QIcon(self.__CONNECT_PNG), self.__CONNECT_ACTION, self)
+        self.__connectAction.setStatusTip(self.__CONNECT_TOOLTIP)
+        self.__connectAction.triggered.connect(self.__openConnectionDialog)
+        return self.__connectAction
+
+    def __createDisconnectAction(self):
+        self.__disconnAction = QAction(QIcon(self.__DISCONNECT_PNG), self.__DISCONNECT_ACTION, self)
+        self.__disconnAction.triggered.connect(self.__disconnect)
+        return self.__disconnAction
+
+    def __createQuitAction(self):
+        quitAction = QAction(QIcon(self.__EXIT_PNG), self.__EXIT_ACTION, self)
+        quitAction.setShortcut(self.__EXIT_SHORTCUT)
+        quitAction.setStatusTip(self.__EXIT_TOOLTIP)
+        quitAction.triggered.connect(self.quit)
+        return quitAction
+
+    def __initMenuBar(self):
+        menubar = self.menuBar()
+        if platform.system() == self.__OSX_NAME:
+            menubar.setNativeMenuBar(False) # Very important for Mac OS X
+        fileMenu = menubar.addMenu(self.__FILE_MENU)
+        fileMenu.addAction(self.__createConnectAction())
+        fileMenu.addAction(self.__createDisconnectAction())
+        fileMenu.addAction(self.__createQuitAction())
+        self.__setConnectionActionsEnabled(True)
+
+    def __openConnectionDialog(self):
+        self.__connDialog = DialogConnectToDataBase()
+        self.__connDialog.onDbConnected.connect(self.saveDbConnection)
+        self.__connDialog.onDbConnectedFailed.connect(self.reportDbConnectionFailure)
+        self.__connDialog.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
