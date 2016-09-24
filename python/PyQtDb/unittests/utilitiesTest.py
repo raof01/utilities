14cc277783a35b9efc7799a6bc10e5b8b6814130
Felix Rao <raof01@gmail.com>
2016-09-24 23:54:53 +0800

    Updated unittests

--- python/PyQtDb/unittests/utilitiesTest.py
+++ python/PyQtDb/unittests/utilitiesTest.py
@@ -2,40 +2,42 @@
 
 import sys
 import unittest
+
 sys.path.append('..')
 import utilities
 
+
 class UtilitiesTest(unittest.TestCase):
-    
     def test_validIpAddress_returnFalse_whenStringContainsIllegalChar(self):
-        self.assertFalse(utilities.validIpAddress('password'))
-    
+        self.assertFalse(utilities.valid_ip_address('password'))
+
     def test_validIpAddress_returnFalse_whenIpAddressOutOfRange(self):
-        self.assertFalse(utilities.validIpAddress('255.256.345.0'))
+        self.assertFalse(utilities.valid_ip_address('255.256.345.0'))
 
     def test_validIpAddress_returnTrue_whenIpAddressIsLocalHost(self):
-        self.assertTrue(utilities.validIpAddress('localhost'))
+        self.assertTrue(utilities.valid_ip_address('localhost'))
 
     def test_validIpAddress_returnTrue_whenIpAddressIsValid(self):
-        self.assertTrue(utilities.validIpAddress('192.168.0.118'))
+        self.assertTrue(utilities.valid_ip_address('192.168.0.118'))
 
     def test_validIpAddress_returnTrue_whenIpAddressIsInValid(self):
-        self.assertFalse(utilities.validIpAddress('192.168.0.'))
+        self.assertFalse(utilities.valid_ip_address('192.168.0.'))
 
     def test_validPort_returnTrue_whenPortIsValid(self):
-        self.assertTrue(utilities.validPort('8889'))
+        self.assertTrue(utilities.valid_port('8889'))
 
     def test_validPort_returnFalse_whenPortStringContainsNoneDigit(self):
-        self.assertFalse(utilities.validPort('88a9'))
+        self.assertFalse(utilities.valid_port('88a9'))
 
     def test_validPort_returnFalse_whenPortIsOutOfRange(self):
-        self.assertFalse(utilities.validPort('-9'))
+        self.assertFalse(utilities.valid_port('-9'))
 
     def test_validPort_returnFalse_whenPortIsOutOfRange(self):
-        self.assertFalse(utilities.validPort('0'))
+        self.assertFalse(utilities.valid_port('0'))
 
     def test_validPort_returnFalse_whenPortIsOutOfRange(self):
-        self.assertFalse(utilities.validPort('65536'))
+        self.assertFalse(utilities.valid_port('65536'))
+
 
 if __name__ == '__main__':
     unittest.main()
