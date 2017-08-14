57e472d6bb3e3fd120aade51c080dbd49dccb0f2
raof01 <raof01@gmail.com>
2017-08-14 11:11:10 +0800

    Update plot-results.rkt

--- python/growth/plot-results.rkt
+++ python/growth/plot-results.rkt
@@ -42,38 +42,51 @@
   (/ (list-min height-periodic-avgs) 2))
 
 (let* ([weight-avg-y-min weight-avg-min-half]
-       [weight-avg-y-max (+ (list-max weight-periodic-avgs) weight-avg-min-half)])
+       [weight-avg-y-max (+ (list-max weight-periodic-avgs) weight-avg-min-half)]
+       [weight-all-avg (car weight-all-avgs)])
   (plot
    (list (tick-grid)
          (lines (map vector (build-day-list (length weight-periodic-avgs)) weight-periodic-avgs)
                 #:label "阶段性日均" #:color 'indigo #:width 1.5)
-         (hrule (car weight-all-avgs)
-                #:label "总体日均" #:color 'teal #:style 'long-dash #:width 1.5))
+         (hrule weight-all-avg
+                #:label (format "总体日均 ~a" (real->decimal-string weight-all-avg))
+                #:color 'teal #:style 'long-dash #:width 1.5))
+   #:legend-anchor 'bottom-left
    #:x-label "天数" #:y-label "体重增长值（克）" #:y-min weight-avg-y-min #:y-max weight-avg-y-max))
 
-(let*  ([weight-y-min (- (list-min weight-interpolated) weight-avg-min-half)]
-       [weight-y-max (+ (list-max weight-interpolated) weight-avg-min-half)])
+(let* ([weight-min (list-min weight-interpolated)]
+       [weight-max (list-max weight-interpolated)]
+       [weight-delta (- weight-max weight-min)]
+       [weight-y-max (+ weight-max (* 20 weight-avg-min-half))])
   (plot
    (list (tick-grid)
          (lines (map vector (build-day-list (length weight-interpolated)) weight-interpolated)
-                #:color 'indigo #:width 1.5))
-   #:x-label "天数" #:y-label "体重（克）" #:y-min weight-y-min #:y-max weight-y-max))
+                #:color 'indigo #:width 1.5
+                #:label (format "当前体重：~a，总增重：~a" weight-max weight-delta)))
+   #:legend-anchor 'bottom-left
+   #:x-label "天数" #:y-label "体重（克）" #:y-min 0 #:y-max weight-y-max))
 
 (let* ([height-avg-y-min height-avg-min-half]
-       [height-avg-y-max (+ (list-max height-periodic-avgs) height-avg-min-half)])
+       [height-avg-y-max (+ (list-max height-periodic-avgs) height-avg-min-half)]
+       [height-all-avg (car height-all-avgs)])
   (plot
    (list (tick-grid)
          (lines (map vector (build-day-list (length height-periodic-avgs)) height-periodic-avgs)
                 #:label "阶段性日均" #:color 'black #:width 1.5)
-         (hrule (car height-all-avgs)
-                #:label "总体日均" #:color 'teal #:style 'long-dash #:width 1.5))
+         (hrule height-all-avg
+                #:label (format "总体日均：~a" (real->decimal-string height-all-avg))
+                #:color 'teal #:style 'long-dash #:width 1.5))
+   #:legend-anchor 'bottom-left
    #:x-label "天数" #:y-label "身高增长值（厘米）" #:y-min height-avg-y-min #:y-max height-avg-y-max))
 
-(let* ([height-y-min (- (list-min height-interpolated) height-avg-min-half)]
-       [height-y-max (+ (list-max height-interpolated) height-avg-min-half)])
+(let* ([height-min (list-min height-interpolated)]
+       [height-max (list-max height-interpolated)]
+       [height-delta (- height-max height-min)]
+       [height-y-max (+ (list-max height-interpolated) (* 20 height-avg-min-half))])
   (plot
    (list (tick-grid)
          (lines (map vector (build-day-list (length height-interpolated)) height-interpolated)
-                #:color 'black #:width 1.5))
-   #:x-label "天数" #:y-label "身高（厘米）" #:y-min height-y-min #:y-max height-y-max))
-
+                #:color 'black #:width 1.5
+                #:label (format "当前身高：~a，总增高：~a" height-max height-delta)))
+   #:legend-anchor 'bottom-left
+   #:x-label "天数" #:y-label "身高（厘米）" #:y-min 0 #:y-max height-y-max))
