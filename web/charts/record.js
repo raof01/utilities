85dc264751c36868878a19f50b33a249baf4d59a
Felix Rao <raof01@gmail.com>
2017-11-29 15:33:32 +0800

    Update growth data

--- web/charts/record.js
+++ web/charts/record.js
@@ -20,6 +20,7 @@ function addRecords(contents) {
     var lines = contents.split("\n");
     var len = lines.length;
     for (var i = 1; i < len; ++i) {
+        if (!lines[i]) { continue; }
 	var words = lines[i].split(",");
 	addRecord(words[0], words[1]);
     }
@@ -38,37 +39,40 @@ function getSeries() {
     var s = [{x: 0, y:records[0].value}];
     var len = records.length;
     for (var i = 1; i < len; ++i) {
-	s[i] = {x: (records[i].recordDate - records[i-1].recordDate) / 1000 / 3600 / 24 + s[i-1].x, y: records[i].value};
+	s[i] = {x: daysBetween(records[i].recordDate, records[i-1].recordDate) + s[i-1].x, y: records[i].value};
 	
     }
     return s;
 }
 
-function plot(contents) {
-    addRecords(contents);
-    
-    var data = {
-	//	labels: getLabels(),
-	series: [
-	    getSeries()
-	]
-    };
-    new Chartist.Line(document.getElementById('c1'), data, {
-	axisX: {
-	    type: Chartist.AutoScaleAxis,
-	    onlyInteger: true
-	},
-	lineSmooth: Chartist.Interpolation.none({
-	    fillHoles: false
-	})
-    });
-    new Chartist.Line(document.getElementById('c2'), data, {
-	axisX: {
-	    type: Chartist.AutoScaleAxis,
-	    onlyInteger: true
-	},
-	lineSmooth: Chartist.Interpolation.none({
-	    fillHoles: false
-	})
-    });
+function daysBetween(endDate, startDate) {
+    return (endDate - startDate) / 1000 / 3600 / 24;
 }
+
+function getAvgSeries() {
+    var s = [{x: 0, y: records[0].value}];
+    var len = records.length;
+    for (var i = 1; i < len; ++i) {
+        var days = daysBetween(records[i].recordDate, records[i-1].recordDate);
+        // var sLen = s.length;
+        var avg = (records[i].value - records[i-1].value) / days;
+        s[i] = {x: days + s[i-1].x, y: avg};
+        if (i === 1) s[0].y = avg;
+    }
+    var s1 = [];
+    for (var i = 0; i < len; ++i) {
+        s1[i] = { x: s[i].x, y: s[i].y };
+    }
+    var allAvg = (records[len - 1].value - records[0].value) / s[len - 1].x;
+    s1.map(e => e.y = allAvg);
+    return [s, s1];
+}
+
+function getAvgLabels(arr) {
+    return arr.map(e => e.x);
+}
+
+function getAvgs(arr) {
+    return arr.map(e => e.y);
+}
+
