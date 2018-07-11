e68cb38d1c63ff7830062642a9134f82ce3b6a7a
Felix Rao <raof01@gmail.com>
2018-07-11 03:15:12 +0000

    Updata filter-dup-jpg.py

--- python/filter-dup-jpg.py
+++ python/filter-dup-jpg.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
-import glob
+import fnmatch
 import os
 from os import path
 import uuid
@@ -29,28 +29,27 @@ def exists(bf, file_name):
 
 
 # TODO: find duplicated files. generate csv recording name mapping between duplicated files
-def process(abs_input_dir, int_dir, abs_dup_dir):
-    bf = BloomFilter(max_elements=21616, error_rate=0.01)
+def process(abs_input_dir, abs_dup_dir):
+    bf = BloomFilter(max_elements=21616 * 2, error_rate=0.01)
     with open('mapping.csv', 'w') as m:
         m.write('old-path-name,new-path-name\n')
-        files = glob.glob(path.join(abs_input_dir, '**/*.jpg'), recursive=True)
-        for f, i in tqdm.tqdm(files):
-            int_name = gen_file_name(f, int_dir)
-            os.rename(f, int_name)
-            dest_name = int_name
-            if exists(bf, int_name):
-                dest_name = path.join(abs_dup_dir, path.basename(int_name))
-                os.rename(int_name, dest_name)
-            m.write(int_name + ',' + dest_name + '\n')
+        files = [v for v in os.walk(abs_input_dir)]
+        for f in tqdm.tqdm(files):
+            for b in tqdm.tqdm(f[2]):
+                if fnmatch.fnmatch(b, '*.jpg'):
+                    name = path.join(f[0], b)
+                    if exists(bf, name):
+                        dest_name = gen_file_name(name, abs_dup_dir)
+                        #print('os.rename('+ name + ', ' + dest_name + ')')
+                        os.rename(name, dest_name)
+                        m.write(name + ',' + dest_name + '\n')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', dest='input_dir', required=True, type=str,
                         help='input dir containing jpg files')
-    parser.add_argument('-o', dest='output_dir', required=True, type=str,
-                        help='output dir containing unique jpg files in input dir')
     parser.add_argument('-d', dest='dup_dir', required=True, type=str,
                         help='dup dir containing duplicated jpg files in input dir')
     args = parser.parse_args()
-    process(args.input_dir, args.output_dir, args.dup_dir)
+    process(args.input_dir, args.dup_dir)
